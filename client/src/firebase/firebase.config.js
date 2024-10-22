ddaeb024b771587fbc99b98c712c21d2cc88b513
Ashutosh1700 <94374532+Ashutosh1700@users.noreply.github.com>
2024-10-22 18:56:30 +0530

    change done

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from "dotenv";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+dotenv.config();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAXRHAxwEjmI7tcIC5933pScPATBeq_m0c",
-    authDomain: "book-inventory-3d703.firebaseapp.com",
-    projectId: "book-inventory-3d703",
-    storageBucket: "book-inventory-3d703.appspot.com",
-    messagingSenderId: "809450123029",
-    appId: "1:809450123029:web:b29adaa5b02d39918c831c",
-    measurementId: "G-NYB602MC3X"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID
 };
 
 // Initialize Firebase
